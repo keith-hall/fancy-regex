d988e264630fcd5e6215b95de2212da498021378
Keith Hall <keith-hall@users.noreply.github.com>
2025-12-27 15:04:40 +0200

    remove no longer needed dead_code attribute on ExprTree

--- src/parse.rs
+++ src/parse.rs
@@ -43,7 +43,6 @@ pub struct ExprTree {
     pub expr: Expr,
     pub backrefs: BitSet,
     pub named_groups: NamedGroups,
-    #[allow(dead_code)]
     pub(crate) contains_subroutines: bool,
     pub(crate) self_recursive: bool,
 }
